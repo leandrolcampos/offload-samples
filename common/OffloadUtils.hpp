ee9a9d5cd08558cbdcdd1d9bef07086ee9d63b13
Leandro Lacerda <15185896+leandrolcampos@users.noreply.github.com>
2025-05-28 13:31:14 -0300

    Add device-query sample

---
+++ common/OffloadUtils.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cstdlib>
+#include <iostream>
+#include <offload/OffloadAPI.h>
+#include <string>
+#include <vector>
+
+namespace ols {
+
+namespace internal {
+
+// OLS_CHECK failure handler. Separated from the macro to ensure this header
+// explicitly includes <cstdlib> and <iostream> for its expansion.
+inline void checkFailureHandler(const char *ResultExpr, ol_result_t Result,
+                                const char *File, int Line,
+                                const char *FuncName) {
+  std::cerr << "OLS_CHECK FAILED: (" << ResultExpr << ") != OL_SUCCESS\n"
+            << "  Error Code: " << Result->Code << '\n'
+            << "  Details: "
+            << (Result->Details ? Result->Details : "No details provided.")
+            << '\n'
+            << "  Location: " << File << ":" << Line << '\n'
+            << "  Function: " << FuncName << '\n';
+  std::exit(EXIT_FAILURE);
+}
+
+} // namespace internal
+
+#define OLS_CHECK(ResultExpr)                                                  \
+  do {                                                                         \
+    ol_result_t Result = (ResultExpr);                                         \
+    if (Result != OL_SUCCESS) {                                                \
+      ols::internal::checkFailureHandler(#ResultExpr, Result, __FILE__,        \
+                                         __LINE__, __func__);                  \
+    }                                                                          \
+  } while (false)
+
+struct Device {
+  ol_device_handle_t Handle;
+  bool IsHost;
+};
+
+struct DeviceInfo {
+  std::string Name;
+  std::string Vendor;
+  std::string DriverVersion;
+};
+
+const std::vector<Device> &getDevices();
+
+const ols::Device &getHostDevice();
+
+DeviceInfo getDeviceInfo(const Device &TargetDevice);
+
+} // namespace ols
